1ac5c864d02fbf9730708006d288ebee4f79cabb
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-09-03 13:26:17 +0200

    basic app

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-let a = 4;
+const express = require("express");
+const morgan = require("morgan");
 
-let b = 5;
+require("dotenv").config();
+
+const app = express();
+
+// Request logging
+app.use(morgan("tiny"));
+
+app.get("/", (req, res, next) => res.send("Hello world!"));
+
+app.listen(Number(process.env.PORT) || 3000, () =>
+  console.log(`App is listening on port ${Number(process.env.PORT) || 3000}.`)
+);
+
+return app;
