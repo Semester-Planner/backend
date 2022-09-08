4080cfab0b096d6f3b1aa55aea1db31715115b85
donnamagi <magi.donna@gmail.com>
2022-09-08 18:43:19 +0200

    feat: route for creating user

---
+++ app/controllers/user.js
@@ -0,0 +1,29 @@
+const db = require("../db/models");
+
+const {
+  User,
+  Sequelize: { Op },
+} = db;
+
+exports.userController = (req, res, next) => {
+  const {
+    body: { username, email, passwordHash },
+  } = req;
+
+  db.User.findOrCreate({
+    where: {
+      [Op.or]: [{ username }, { email }],
+    },
+    defaults: {
+      username: username,
+      email: email,
+      passwordHash: passwordHash,
+    },
+  })
+    .then(([user, created]) => {
+      console.log(user);
+      if (!created) throw Error("Name or email already in use");
+      return res.status(200).send("Successfully created user");
+    })
+    .catch((err) => next(err));
+};
--- app/routes/main.js
+++ app/routes/main.js
@@ -9,6 +9,7 @@ const { testController } = require("../controllers/main");
 router.get("/", (_req, res, _next) => res.send("Hello world"));
 
 router.post("/hello", testController);
+router.post("/user/createUser", userController);
 
 router.use(unresolvedPathHandler);
 router.use(errorHandler);
