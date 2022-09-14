c234bd5636d611a2bf619ba9bc108c95e658aea9
Alsje <alsje@mernok.co.za>
2022-09-14 11:00:59 +0200

    feature: created module model (migration incl)

---
+++ app/db/migrations/0-create-module.js
@@ -0,0 +1,39 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Module", {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      mod_code: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      department: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      mandatory: {
+        type: Sequelize.BOOL,
+        allowNull: false,
+      },
+      coordinator: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Module");
+  },
+};
