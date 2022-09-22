204f3e2e7e92b51029f1eefa02f7015c288919ae
Alsje <alsje@mernok.co.za>
2022-09-22 18:20:45 +0200

    feat: new create-requirement migration and requirement model (no associations)

---
+++ app/db/migrations/2-create-requirement.js
@@ -0,0 +1,28 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Requirement", {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.literal("uuid_generate_v4()"),
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Requirement");
+  },
+};
