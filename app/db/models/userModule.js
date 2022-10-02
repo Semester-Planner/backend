c07425e49b4d69584e5764b971483a583526e6d9
Alsje <alsje@mernok.co.za>
2022-10-02 11:02:31 +0200

    update: fixed entry/userModule association

--- app/db/models/userModule.js
+++ app/db/models/userModule.js
@@ -3,8 +3,7 @@ const { Model } = require("sequelize");
 module.exports = (sequelize, Sequelize) => {
   class UserModule extends Model {
     static associate(models) {
-      this.belongsTo(models.Entry, {
-        through: models.Entry,
+      this.hasMany(models.Entry, {
         foreignKey: "userModuleId",
       });
     }
