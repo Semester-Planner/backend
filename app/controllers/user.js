d5eb169e4a5f87089fc3d895d66b718403cf8389
Alsje <alsje@mernok.co.za>
2022-10-22 12:54:45 +0200

    refactor: cleaned all controllers

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -3,47 +3,6 @@ const db = require("../db/models");
 const { User } = db;
 const { Module } = db;
 
-// create user
-exports.createUser = (req, res, next) => {
-  const {
-    body: { name, surname, email, passwordHash },
-  } = req;
-
-  db.User.findOrCreate({
-    where: { email },
-    defaults: {
-      name: name,
-      surname: surname,
-      passwordHash: passwordHash,
-    },
-  })
-    .then(([user, created]) => {
-      if (!created) throw Error("Email already in use");
-      return res.status(200).send("Successfully created user");
-    })
-    .catch((err) => next(err));
-};
-
-// reset password
-exports.resetPassword = (req, res, next) => {
-  const {
-    body: { email, oldPassword, newPassword },
-  } = req;
-
-  User.findByEmail(email)
-    .then((user) => {
-      if (user.passwordHash === oldPassword) {
-        user.passwordHash = newPassword;
-        return user.save();
-      }
-      throw Error("Incorrect password");
-    })
-    .then(() => {
-      return res.status(200).send("Password changed successfully :)");
-    })
-    .catch((err) => next(err));
-};
-
 // find all user's modules
 exports.findAllUserModules = (req, res, next) => {
   const {
@@ -52,7 +11,7 @@ exports.findAllUserModules = (req, res, next) => {
 
   User.findAll({
     where: { id: userId },
-    attributes: { exclude: ["name", "surname", "email", "passwordHash"] },
+    attributes: { exclude: ["name", "surname", "email", "picture"] },
     include: [
       {
         model: Module,
