027993a29a38169932f88ed1e326a2e90109cc0a
Alsje <alsje@mernok.co.za>
2022-10-25 22:30:58 +0200

    feature: new addModule, createEntry, getAllEntries and getAllModuleReqs & refactor: deleted user routes

--- app/routes/main.js
+++ app/routes/main.js
@@ -9,17 +9,17 @@ const checkAuth = require("../middlewares/auth");
 
 // import all routers
 const authRoutes = require("./auth.routes");
-const userRoutes = require("./user.routes");
 const moduleRoutes = require("./module.routes");
 const entryRoutes = require("./entry.routes");
+const requirementRoutes = require("./requirement.routes");
 
 // main routes
 router.use("/auth", authRoutes);
 // !-- commented for testing:
 //router.use(checkAuth);
-router.use("/user", userRoutes);
 router.use("/module", moduleRoutes);
 router.use("/entry", entryRoutes);
+router.use("/requirement", requirementRoutes);
 
 router.use(unresolvedPathHandler);
 router.use(errorHandler);
--- app/routes/module.routes.js
+++ app/routes/module.routes.js
@@ -5,10 +5,12 @@ const {
   createModule,
   getAllUserModules,
   getAllModules,
+  addModule,
 } = require("../controllers/module");
 
 router.get("/getAllModules", getAllModules);
 router.get("/getAllUserModules", getAllUserModules);
 router.post("/createModule", createModule);
+router.post("/addModule", addModule);
 
 module.exports = router;
