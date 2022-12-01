ad0c7d5e7c32613a18759e95357367e6211bde6d
Alsje <alsje@mernok.co.za>
2022-12-01 18:54:18 +0100

    refactor: removed redundant comments from main routes

--- app/routes/main.js
+++ app/routes/main.js
@@ -1,23 +1,19 @@
 const router = require("express").Router();
 
-// import middlewares
 const {
   unresolvedPathHandler,
   errorHandler,
 } = require("../middlewares/error_handlers");
 const checkAuth = require("../middlewares/auth");
 
-// import all routers
 const authRoutes = require("./auth.routes");
 const userRoutes = require("./user.routes");
 const moduleRoutes = require("./module.routes");
 const entryRoutes = require("./entry.routes");
 const requirementRoutes = require("./requirement.routes");
 
-// main routes
 router.use("/auth", authRoutes);
-// !-- commented for testing:
-//router.use(checkAuth);
+router.use(checkAuth);
 router.use("/users", userRoutes);
 router.use("/module", moduleRoutes);
 router.use("/entry", entryRoutes);
