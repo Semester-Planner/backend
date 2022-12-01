9a899d6568f251c62f58fab3cfb76b72af46c6db
Alsje <alsje@mernok.co.za>
2022-12-01 18:30:38 +0100

    refactor: remove redundant comments and unused var from strategies

--- app/passport/strategies.js
+++ app/passport/strategies.js
@@ -1,18 +1,15 @@
 const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, PORT, NODE_ENV } = process.env;
 
-const db = require("../db/models");
 const { createUser } = require("../controllers/user");
-
-const { User } = db;
 
 var Strategy =
   NODE_ENV != "test"
     ? require("passport-google-oauth20").Strategy
     : require("passport-mocked").Strategy;
 /**
- * registers all passport authentication strategiess
+ * registers all passport authentication strategies
  *
- * @param {Object} passport - passport object
+ * @param {Object} passport
  */
 exports.register = async (passport) => {
   const callbackURL = `http://localhost:${PORT || "3001"}/auth/google/callback`;
@@ -27,7 +24,6 @@ exports.register = async (passport) => {
         callbackURL,
       },
       async (_accessToken, _refreshToken, data, done) => {
-        // find or create user email in database
         const user = await createUser(data);
         if (user) return done(null, user);
         return done();
