9e10c7881c25e4baf11c888eb2c654d202c7b3bd
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-12-05 10:20:26 +0100

    noauth debug middleware

--- app/routes/main.js
+++ app/routes/main.js
@@ -12,8 +12,24 @@ const moduleRoutes = require("./module.routes");
 const entryRoutes = require("./entry.routes");
 const requirementRoutes = require("./requirement.routes");
 
+// debug middleware, temp
+if (process.env.NOAUTH === "true") {
+  router.use((req, _res, next) => {
+    const db = require("../db/models");
+    const { User } = db;
+
+    User.findOne({
+      where: { name: "Donna" },
+    })
+      .then((user) => {
+        req.user = user;
+        next();
+      });
+  });
+}
+
 router.use("/auth", authRoutes);
-// router.use(checkAuth);
+router.use(checkAuth);
 router.use("/users", userRoutes);
 router.use("/module", moduleRoutes);
 router.use("/entry", entryRoutes);
